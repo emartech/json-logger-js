2b920591c64b56d7ec937378ae72440fe931e4de
D056966 <steffen.schroeder01@sap.com>
2025-05-09 12:27:19 +0200

    format

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,23 +1,23 @@
 import { expect } from 'chai';
 import { createLogger, Logger } from './index';
 
-describe('createLogger', function() {
-  beforeEach(function() {
+describe('createLogger', function () {
+  beforeEach(function () {
     process.env.DEBUG = 'mongo';
   });
 
-  afterEach(function() {
+  afterEach(function () {
     process.env.DEBUG = '';
   });
 
-  it('should return an enabled log instance when env variable is set to same', function() {
+  it('should return an enabled log instance when env variable is set to same', function () {
     const logger = createLogger('mongo');
 
     expect(logger).to.be.an.instanceOf(Logger);
     expect(logger.isEnabled()).to.be.true;
   });
 
-  it('should return a disabled log instance when different', function() {
+  it('should return a disabled log instance when different', function () {
     const logger = createLogger('redis');
 
     expect(logger).to.be.an.instanceOf(Logger);
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,18 @@
+import { isNamespaceEnabled } from './enabled/enabled';
+import { formatter } from './formatter';
 import { Logger, LoggerConfig } from './logger/logger';
 export { Logger, LoggerConfig } from './logger/logger';
 export { Timer } from './timer/timer';
-import { isNamespaceEnabled } from './enabled/enabled';
-import { formatter } from './formatter';
 
+// eslint-disable-next-line func-style
 export function createLogger(namespace: string): Logger {
-  return new Logger(namespace, isNamespaceEnabled(
-    createLogger.getNamespaces(), namespace
-  ));
+  return new Logger(namespace, isNamespaceEnabled(createLogger.getNamespaces(), namespace));
 }
 
-createLogger.getNamespaces = function(): string {
-  return process.env.DEBUG || '';
+createLogger.getNamespaces = function (): string {
+  return process.env.DEBUG ?? '';
 };
-createLogger.configure = function(options: Partial<LoggerConfig>): void {
+createLogger.configure = function (options: Partial<LoggerConfig>): void {
   Logger.configure(options);
 };
 createLogger.formatter = formatter;
